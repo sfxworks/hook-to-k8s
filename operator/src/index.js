37ccc64915b2bb5c45a9bc31d5c334d37c1989b1
Samuel Walker <services@sfxworks.net>
2019-05-11 16:14:51 -0400

    Dump

--- operator/src/index.js
+++ operator/src/index.js
@@ -1,9 +1,19 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/' + process.env.WEBHOOK_PATH, secret: process.env.WEBHOOK_SECRET })
+//var handler = createHandler({ path: '/' + process.env.WEBHOOK_PATH, secret: process.env.WEBHOOK_SECRET })
+var handler = createHandler({ path: '/webhook', secret: 'supersecret' })
 
-const k8s = require('@kubernetes/client-node');
+const K8s = require('easy-k8s').Client;
+const config = require('kubernetes-client').config
+kubeconfig = config.getInCluster()
 
+async function testPod()
+{
+  const payloadSpec = await K8s.get(kubeconfig, 'default', 'payloads');
+  console.log(payloadSpec)
+}
+
+testPod();
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
@@ -16,16 +26,25 @@ handler.on('error', function (err) {
   console.error('Error:', err.message)
 })
  
-handler.on('push', function (event) {
+handler.on('push', async function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref)
-})
+    /*
+    const create = await client.apis["hook-to-k8s.sfxworks.net"].v1.namespaces.default.payloads.post({ body: {
+      "apiVersion": "hook-to-k8s.sfxworks.net/v1",
+      "kind": "Payload",
+      "metadata": {
+        "type": "github",
+        "name": event.payload.repository.name,
+        "sha": event.payload.after,
+        "head commit author": event.payload.head_commit.author.name
+      },
+      "spec": {
+        "payload": event.payload
+      }
+    }})
+    */
 
-console.log("Initialized.")
-
-const watch = new k8s.Watch(config)
-watch.watch('api/v1/namespaces', {}, (phase, obj) => {
-  
+    console.log(create)
 })
-///test
